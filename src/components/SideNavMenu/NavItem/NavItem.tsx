79816e5d4c2bf07aa5996a5456bab36d9a28c3cf
Reynier Perez <reynierpm@gmail.com>
2023-06-05 20:49:46 -0400

    Adding onClick handlers

--- src/components/SideNavMenu/NavItem/NavItem.tsx
+++ src/components/SideNavMenu/NavItem/NavItem.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavItemProps } from '../SideNavMenu.model';
 import ChevronDownIcon from '../Icon/ChevronDownIcon';
 
-const NavItem = ({ item }: { item: NavItemProps<string> }) => {
+const NavItem = ({
+	item,
+	onItemClick,
+	isShown = false,
+}: {
+	item: NavItemProps<string>;
+	onItemClick: any;
+	isShown?: boolean | undefined;
+}) => {
 	const hasSubNav = item.subNav && item.subNav.length > 0;
 	const subNav = item.subNav;
 
 	return (
 		<li className="side-navigation-panel-select-wrap">
-			<div>
+			<div onClick={() => onItemClick(item)}>
 				<span className="side-navigation-panel-select-option-wrap">{item.title}</span>
 				{hasSubNav ? <ChevronDownIcon /> : ''}
 			</div>
-			{hasSubNav && (
+			{hasSubNav && isShown && (
 				<ul className="side-navigation-panel-select">
 					{subNav?.map((item: NavItemProps<string>) => {
-						return <NavItem key={item.path} item={item} />;
+						return <NavItem key={item.path} item={item} onItemClick={onItemClick} />;
 					})}
 				</ul>
 			)}
--- src/components/SideNavMenu/SideNavMenu.tsx
+++ src/components/SideNavMenu/SideNavMenu.tsx
@@ -3,7 +3,50 @@ import { NavItemProps } from './SideNavMenu.model';
 import './style.scss';
 import NavItem from './NavItem/NavItem';
 
-const SideNavMenu = ({ items }: { items: NavItemProps<string>[] }) => {
+const SideNavMenu = ({
+	items,
+	activePath = '',
+}: {
+	items: NavItemProps<string>[];
+	activePath?: string;
+}) => {
+	const [activeItem, setActiveItem] = useState({
+		expanded: false,
+		path: activePath,
+	});
+
+	// Listen for parent prop changes and update state
+	React.useEffect(() => {
+		setActiveItem((originalSubNav) => ({
+			expanded: originalSubNav.expanded,
+			path: activePath,
+		}));
+	}, [activePath]);
+
+	const handleClick = (item: NavItemProps<string>) => {
+		console.log('click', activeItem.expanded, activeItem.path);
+		const hasSubNav = item.subNav && item.subNav.length > 0;
+		const subNav = item.subNav;
+
+		if (activeItem.expanded) {
+			const currentItemIsOpen: boolean =
+				item.path === activeItem.path ||
+				(hasSubNav &&
+					subNav?.some((_subNavItem) => _subNavItem.path === activeItem.path)) ||
+				false;
+
+			setActiveItem({
+				expanded: hasSubNav ? !currentItemIsOpen : false,
+				path: item.path,
+			});
+		} else {
+			setActiveItem({
+				expanded: !!hasSubNav && item.path === activeItem.path,
+				path: item.path,
+			});
+		}
+	};
+
 	return (
 		<>
 			{items.length > 0 && (
@@ -14,7 +57,14 @@ const SideNavMenu = ({ items }: { items: NavItemProps<string>[] }) => {
 				>
 					<ul className="side-navigation-panel-select">
 						{items.map((item: NavItemProps<string>, key: number) => {
-							return <NavItem key={item.path} item={item} />;
+							return (
+								<NavItem
+									key={item.path}
+									item={item}
+									onItemClick={handleClick}
+									isShown={activeItem.expanded}
+								/>
+							);
 						})}
 					</ul>
 				</nav>
