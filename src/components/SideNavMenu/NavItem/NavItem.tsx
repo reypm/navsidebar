50740a21cf26bb76a4d9147ad01cd830119d4c81
Reynier Perez <reynierpm@gmail.com>
2023-06-06 12:20:43 -0400

    Added CSS styling

--- src/components/SideNavMenu/NavItem/NavItem.tsx
+++ src/components/SideNavMenu/NavItem/NavItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavItemProps } from '../SideNavMenu.model';
 import ChevronDownIcon from '../Icon/ChevronDownIcon';
+import ChevronUpIcon from '../Icon/ChevronUpIcon';
 
 const NavItem = ({
 	item,
@@ -15,13 +16,22 @@ const NavItem = ({
 	const subNav = item.subNav;
 
 	return (
-		<li className="side-navigation-panel-select-wrap">
-			<div onClick={() => onItemClick(item)}>
+		<li
+			className={`side-navigation-panel-select-wrap ${
+				hasSubNav ? 'side-navigation-panel-select-inner-wrap' : ''
+			}`}
+		>
+			<div
+				onClick={() => onItemClick(item)}
+				className={`side-navigation-panel-select-option ${
+					isShown.includes(item.id) ? 'side-navigation-panel-select-option-selected' : ''
+				}`}
+			>
 				<span className="side-navigation-panel-select-option-wrap">{item.title}</span>
 				{hasSubNav ? <ChevronDownIcon /> : ''}
 			</div>
 			{hasSubNav && isShown.includes(item.id) && (
-				<ul className="side-navigation-panel-select">
+				<ul className="side-navigation-panel-select-inner">
 					{subNav?.map((item: NavItemProps<string>) => {
 						return (
 							<NavItem
